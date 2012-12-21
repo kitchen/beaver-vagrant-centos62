3b7e18e6abdc52d7a550045d4adc7bf685f39686
Jeremy Kitchen <jeremy.kitchen@gorillanation.com>
2012-12-20 23:46:37 -0800

    removing commented bits of manifest

--- manifests/centos62.pp
+++ manifests/centos62.pp
@@ -19,11 +19,3 @@ package { "beaver":
     source => "git+git://github.com/josegonzalez/beaver.git#egg=beaver",
     require => [Package["python-pip"],File["/usr/bin/pip"],Package["gcc"]],
 }
-
-#exec { "beaver_install":
-#command => "/usr/bin/pip-python install git+git://github.com/josegonzalez/beaver.git#egg=beaver",
-#creates => "/usr/bin/beaver",
-#require => [Package["python-pip"],Package["git"]],
-#}
-
-    
